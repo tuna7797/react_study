537d3f17331c75e2c75bc8126addcd2a45704184
tuna7797 <gmlwjd7731@naver.com>
2022-05-14 22:09:08 +0900

    기본 JSX 가상돔 생성

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
+/*
+	컴포넌트 함수는 한번에 하나의 JSX만 반환가능
+	복수개의 요소를 반환할때에는 wrapping tag로 감싸줌
+	불필요한 부모태그의 생성을 막기 위해서는 <></> (fragment)로 감싸줌
+*/
+
 function App() {
 	return (
-		<h1>Hello</h1>
+		<>
+			<header>
+				<h1>LOGO</h1>
+
+				<a href="#">menu</a>
+			</header>
+
+			<footer>
+				<p>2022 JOEHUIJEONG &copy; ALL RIGHTS RESERVED.</p>
+			</footer>
+		</>
 	);
 }
 
